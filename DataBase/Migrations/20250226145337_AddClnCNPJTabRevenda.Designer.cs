b08892489e093cad9e7de3e8f38a9ae368931489
Victor Notari <victor@casainfosc.com>
2025-02-26 14:53:02 -0300

    - Adicionados endpoints na `RevendaController` para atualizar a situação de uma revenda de forma independente. - Criado o modelo `AtualizarSituacaoRevenda` para atualizar a situação de uma revenda. - Melhorias gerais na documentação dos endpoints.

--- DataBase/Migrations/20250226145337_AddClnCNPJTabRevenda.Designer.cs
+++ DataBase/Migrations/20250226145337_AddClnCNPJTabRevenda.Designer.cs
@@ -4,6 +4,7 @@ using DataBase.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataBase.Migrations
 {
     [DbContext(typeof(MyServiceStoreDBContext))]
-    partial class MyServiceStoreDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250226145337_AddClnCNPJTabRevenda")]
+    partial class AddClnCNPJTabRevenda
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Modelos/ModelosRequest/Revenda/AtualizarSituacaoRevenda.cs
+++ Modelos/ModelosRequest/Revenda/AtualizarSituacaoRevenda.cs
@@ -1,5 +1,4 @@
-﻿using Modelos.EF.Entidade;
-using Modelos.Enuns;
+﻿using Modelos.Enuns;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +7,8 @@ using System.Threading.Tasks;
 
 namespace Modelos.ModelosRequest.Revenda
 {
-    public class CriarRevenda
+    public class AtualizarSituacaoRevenda
     {
-        public int EntidadeId { get; set; }
-        public string CNPJ { get; set; }
         public Situacao Situacao { get; set; }
     }
 }
