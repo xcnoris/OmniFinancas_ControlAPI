eb7acf78f59311a03232bedf31259bd8842eb5c7
Augusto Nascimento <augustobriel12@gmail.com>
2025-03-01 13:04:39 -0300

    Sincroninzando

--- Modelos/EF/SoftwaresModel.cs
+++ Modelos/EF/SoftwaresModel.cs
@@ -1,5 +1,6 @@
 ﻿using Modelos.EF.Revenda;
 using Modelos.Enuns;
+using System.Text.Json.Serialization;
 
 namespace Modelos.EF
 {
@@ -10,6 +11,7 @@ namespace Modelos.EF
         public string Descrição { get; set; }
         public string Versao { get; set; }
         public int ProprietarioId { get; set; }
+        [JsonIgnore] // Evita o ciclo
         public virtual RevendaModel? Proprietario { get; set; }
         public Situacao Situacao{ get; set; }
         public DateTime DataCriacao { get; set; }
